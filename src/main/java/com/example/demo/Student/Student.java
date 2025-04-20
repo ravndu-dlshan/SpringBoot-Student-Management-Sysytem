85e323185c3b13be6bccd889357cf8af289e0b91
ravndu-dlshan <159864490+ravndu-dlshan@users.noreply.github.com>
2025-04-20 14:55:58 +0530

    Final Commit

--- src/main/java/com/example/demo/Student/Student.java
+++ src/main/java/com/example/demo/Student/Student.java
@@ -63,6 +63,15 @@ public class Student {
         return Period.between(this.dob, LocalDate.now()).getYears();
     }
 
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+
     @Override
     public String toString() {
         return "Student{" +
@@ -73,4 +82,7 @@ public class Student {
                 ", age=" + getAge() +
                 '}';
     }
+
+
+
 }
--- src/main/java/com/example/demo/Student/StudentController.java
+++ src/main/java/com/example/demo/Student/StudentController.java
@@ -1,9 +1,8 @@
 package com.example.demo.Student;
 
+import jakarta.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -19,7 +18,26 @@ public class StudentController {
     }
 
     @GetMapping
-    public List<Student> getStudents(){
+    public List<Student> getStudents() {
         return studentService.getStudents();
+    }
+
+    @PostMapping
+    public void registerNewStudent(@RequestBody Student student) {
+        studentService.addNewStudent(student);
+    }
+
+    @DeleteMapping(path = "{studentId}")
+    public void deleteStudent(@PathVariable("studentId") Long ID) {
+
+        studentService.deleteStudent(ID);
+    }
+
+    @PutMapping(path = "{studentId}")
+    public void updateStudent(
+            @PathVariable("studentId") Long studentId,
+            @RequestParam(required = false) String name,
+            @RequestParam(required = false) String email){
+        studentService.updateStudent(studentId,name,email);
     }
 }
